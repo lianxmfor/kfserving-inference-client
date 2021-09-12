483f905533bd3efd5de6f9df2175fac11f6fc083
lianxmfor <1034552569@qq.com>
2021-09-12 13:50:19 +0800

    新增测试文件

--- mapping/mapping_test.go
+++ mapping/mapping_test.go
@@ -1,5 +1,17 @@
 package mapping
 
-func TestMapping() {
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMapping(t *testing.T) {
 	Init(".")
+
+	fmt.Println(len(mapping))
+	assert.NotEqual(t, 0, len(mapping))
+
+	assert.Equal(t, float64(1), mapping["city"]["beijing"])
 }
